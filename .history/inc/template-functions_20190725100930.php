7f03ab80c51ef710fd4d8bd985ac124bd65e0a8a
Maximo Sojo <maxsojo13@gmail.com>
2019-07-26 09:48:54 -0400

    refactor(History): Se remueve carpeta de historial.

--- .history/inc/template-functions_20190725100930.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Functions which enhance the theme by hooking into WordPress
- *
- * @package Website
- * @since 1.0.0
- */
--- .history/inc/template-functions_20190726093626.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Functions which enhance the theme by hooking into WordPress
- *
- * @package Website
- * @author Máximo Sojo
- * @since 1.0.0
- */
